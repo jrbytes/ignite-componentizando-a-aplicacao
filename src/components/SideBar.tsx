983f980947992e2549997f425dc8fb638b19836b
Jr Bytes <itamazor@gmail.com>
2021-03-31 19:21:12 -0300

    Updated app to handle content and sidebar how component.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+type GenreResponseProps = {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}[]
+
+type SideBarProps = {
+  genres: GenreResponseProps
+  selectedGenreId: number
+  setSelectedGenreId: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
